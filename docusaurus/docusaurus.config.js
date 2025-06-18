bd22abe71440ab8f471bd30dc467f171fd261442
Adam Hewitt <ahewitt@glasswall.com>
2025-06-18 02:18:43 +0100

    update baseurl for github pages

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://werzl.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/files/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
